703eecf8fffd2f7189dce80c58386d2cad741b39
Chentuantang <139760440+Chentuantang@users.noreply.github.com>
2025-04-16 11:38:17 +0800

    Initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/www1/catkin_ws/src
+# Build directory: /home/www1/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("anno_description")
---
+++ build/Urdf_PRobotAnno/anno_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/www1/catkin_ws/src/Urdf_PRobotAnno/anno_description
+# Build directory: /home/www1/catkin_ws/build/Urdf_PRobotAnno/anno_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
